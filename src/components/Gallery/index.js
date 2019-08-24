f25b8083d562f764628e8e92054205b54a0a9d99
mohammad abed <mabed4297@gmail.com>
2019-08-24 17:22:18 +0300

    dev: add image gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,17 +2,41 @@ import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 import "./Gallery.css"
-export default function Gallery({ thumbnails }) {
+import { Row, Col } from "reactstrap"
+export default function Gallery({ thumbnails, photos }) {
+  const [activeIndex, setActiveIndex] = React.useState(0)
+  React.useEffect(() => {
+    console.log(photos)
+  }, [photos])
   return (
     <div>
       <div className="thumbnails__container">
-        {thumbnails.allFile.edges.map(thumbnail => {
+        {thumbnails.edges.map(thumbnail => {
           return (
             <div
               key={thumbnail.node.id}
               className="thumbnails__container__thumbnail"
             >
               <Img fluid={thumbnail.node.childImageSharp.fluid} />
+            </div>
+          )
+        })}
+      </div>
+      <div className="gallery__container__scrollable">
+        {photos[0].nodes.map(node => {
+          return (
+            <div className="gallery__container__scrollable__image">
+              <Img fluid={node.childImageSharp.fluid} />
+            </div>
+          )
+        })}
+      </div>
+
+      <div className="gallery__container">
+        {photos[0].nodes.map((node, index) => {
+          return (
+            <div className={`gallery__container__image--${index}`}>
+              <Img fluid={node.childImageSharp.fluid} />
             </div>
           )
         })}
--- src/pages/spotlight.js
+++ src/pages/spotlight.js
@@ -1,35 +1,42 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { graphql, useStaticQuery } from "gatsby"
-import BackgroundImage from "gatsby-background-image"
-import Button from "../components/button"
+import { graphql } from "gatsby"
 import Gallery from "../components/Gallery"
 
-const SpotLight = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allFile(
-        filter: { name: { regex: "/^thumbnail/" } }
-        sort: { order: ASC, fields: name }
-      ) {
-        edges {
-          node {
-            id
-            name
-            ...thumbnailImage
-          }
-        }
-      }
-    }
-  `)
-
+const SpotLight = ({ data }) => {
   return (
     <Layout>
       <SEO title="Spotlight" />
-      <Gallery thumbnails={data} />
+      <Gallery thumbnails={data.thumbnails} photos={[data.photos]} />
     </Layout>
   )
 }
 
+export const query = graphql`
+  query myQuery {
+    thumbnails: allFile(
+      filter: { name: { regex: "/^thumbnail/" } }
+      sort: { order: ASC, fields: name }
+    ) {
+      edges {
+        node {
+          id
+          name
+          ...thumbnailImage
+        }
+      }
+    }
+
+    photos: allFile(filter: { name: { regex: "/^malakalhusseini/" } }) {
+      nodes {
+        childImageSharp {
+          fluid(maxWidth: 400, maxHeight: 300) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  }
+`
 export default SpotLight

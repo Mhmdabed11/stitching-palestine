35ab632816bbd2d6056e579d05977936f15324d1
Mohammad <mohd.abed@saugo360.com>
2020-02-04 21:30:03 +0200

    fix bloating image on homepage

--- src/components/stitchBackground/index.js
+++ src/components/stitchBackground/index.js
@@ -10,7 +10,7 @@ export default function StitchBackground() {
         id
         childImageSharp {
           fluid {
-            ...GatsbyImageSharpFluid_withWebp
+            ...GatsbyImageSharpFluid
           }
         }
       }

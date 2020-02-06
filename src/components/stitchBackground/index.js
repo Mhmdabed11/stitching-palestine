0a7f550e0dea9381c03c4285c07300babb7fffa8
Mohammad <mohd.abed@saugo360.com>
2020-02-06 17:43:43 +0200

    add credits

--- src/components/stitchBackground/index.js
+++ src/components/stitchBackground/index.js
@@ -18,21 +18,35 @@ export default function StitchBackground() {
   `)
 
   return (
-    <div className="stitchbg__container">
-      <div className="stitch__bg__container__title">
-        With every stitch <br /> the women are hiding <br /> their stories
-      </div>
-      <div>
-        <Link to="/women">
-          <Button>Hear their Stories</Button>
-        </Link>
-      </div>
+    <>
+      <div className="stitchbg__container">
+        <div className="stitch__bg__container__title">
+          With every stitch <br /> the women are hiding <br /> their stories
+        </div>
+        <div>
+          <Link to="/women">
+            <Button>Hear their Stories</Button>
+          </Link>
 
-      <Image
-        fluid={data.file.childImageSharp.fluid}
-        className="stitchbg__container__map__bg"
-        imgStyle={{ width: "100%", height: "100%", objectFit: "contain" }}
-      />
-    </div>
+          <Image
+            fluid={data.file.childImageSharp.fluid}
+            className="stitchbg__container__map__bg"
+            imgStyle={{ width: "100%", height: "100%", objectFit: "contain" }}
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          padding: "16px",
+          marginTop: -50,
+          fontWeight: 500,
+        }}
+      >
+        "Twelve Windows" of Palestine is an embroidery project conceived and
+        executed by <br /> INAASH (Association for the Development of
+        Palestinian Camps) and funded by TAAWON - Lebanon.
+      </div>
+    </>
   )
 }
